6c07bded61f0534fa975f8691037f3b45447073b
psingularity <lydia.alexandrowna@gmail.com>
2022-11-04 22:32:13 +0300

    style: removing extra spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   def test_passing(test_level)	 	
-  	 Test.joins('JOIN users ON tests.author_id = users.id')
-  	 	   .where('level = :level AND author_id = :author_id', level: test_level, author_id: user.id)
-  	 	   .order('created_at ASC')
+  	Test.joins('JOIN users ON tests.author_id = users.id')
+        .where('level = :level AND author_id = :author_id', level: test_level, author_id: user.id)
+        .order('created_at ASC')
   end	
 end
 
