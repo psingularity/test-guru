6a8fcf1d8cbaf21fc4ab7cdda7bc2fbc81dc3b0d
psingularity <lydia.alexandrowna@gmail.com>
2022-11-08 04:52:19 +0300

    fix: edited code for user tests by level, added user_tests model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-  def test_passing(test_level)	 	
-  	Test.joins('JOIN users ON tests.author_id = users.id')
-        .where('level = :level AND author_id = :author_id', level: test_level, author_id: user.id)
-        .order('created_at ASC')
-  end	
+  def user_tests(level)
+    Test.joins('JOIN users_tests ON tests.id = users_tests.test_id')
+         .where('tests.level = :level AND users_tests.user_id = :user_id',
+                level: level, user_id: id)
+  end
 end
-
-
-	
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,106 +12,106 @@
 #   Question :body, :test_id
 #   Answer  :body, :correct, :question_id
 
-User.create ([
+@users = User.create ([
   { name: 'User1', age: 21, email: 'test@user1.com' },
   { name: 'User2', age: 31, email: 'test@user2.com' },
   { name: 'User3', age: 48, email: 'test@user3.com' },
   { name: 'User4', age: 31, email: 'test@user4.com' }
 ])
 
-Category.create ([
+@categories = Category.create ([
   { title: 'Frontend' },
   { title: 'Backend' },
   { title: 'Mobile' },
 ])
 
-Test.create ([
+@tests = Test.create ([
   { 
-    title: 'Ruby', 
+  	title: 'Ruby', 
 	level: 0, 
-	category_id: Category.where(title: 'Backend')[0].id, 
-	author_id: User.where(name: "User4")[0].id
+	category_id: @categories[1].id, 
+	author_id: @users[3].id
   },
   {
    	title: 'JS', 
 	level: 2, 
-	category_id: Category.where(title: 'Frontend')[0].id, 
-	author_id: User.where(name: "User2")[0].id
+	category_id: @categories[0].id, 
+	author_id:  @users[1].id
   },
   {
    	title: 'JS 0 Level', 
 	level: 0, 
-	category_id: Category.where(title: 'Frontend')[0].id, 
-	author_id: User.where(name: "User4")[0].id
+	category_id: @categories[0].id, 
+	author_id: @users[3].id
   },
   {
     title: 'HTML', 
 	level: 1, 
-	category_id: Category.where(title: 'Mobile')[0].id, 
-	author_id: User.where(name: "User3")[0].id 
+	category_id: @categories[2].id, 
+	author_id:  @users[2].id 
   },
    {
     title: 'HTML', 
 	level: 1, 
-	category_id: Category.where(title: 'Mobile')[0].id, 
-	author_id: User.where(name: "User3")[0].id 
+	category_id: @categories[2].id, 
+	author_id: @users[2].id 
   }
 ])
 
-Question.create ([
+@questions = Question.create ([
   { 
   	body: 'What does HTML stand for?', 
-  	test_id: Test.where(title: 'HTML')[0].id
+  	test_id: @tests[3].id
   },
   { 
   	body: 'Inside which HTML element do we put the JavaScript?',
-  	test_id: Test.where(title: 'JS')[0].id
+  	test_id: @tests[2].id
   },
   { 
   	body: 'Consider the following irb quotes. After = > is the irb response. Which of the following answers is wrong?', 
-  	test_id: Test.where(title: 'Ruby')[0].id
+  	test_id: @tests[0].id
   }
 ])
 
 Answer.create ([
   { 
     body: 'Hyperlinks and Text Markup Language', 
-    question_id: Question.first.id
+    question_id: @questions[0].id
   },
   { 
   	body: 'Home Tool Markup Language', 
-  	question_id: Question.first.id
+  	question_id: @questions[0].id
   },
   { 
   	body: 'Hyper Text Markup Language', 
   	correct: true, 
-  	question_id: Question.first.id
+  	question_id: @questions[0].id
   },
   { 
   	body: '<javascript>',
-  	question_id: Question.second.id
+  	question_id: @questions[1].id
   },
   { 
   	body: '<p>', 
-  	question_id: Question.second.id
+  	question_id: @questions[1].id
   },
   { 
   	body: '<script>', 
   	correct: true, 
-  	question_id: Question.second.id
+  	question_id: @questions[1].id
   },
   { 
   	body: "irb(main):001:0> 'gin joint'.length => 9", 
-  	question_id: Question.last.id
+  	question_id: @questions[2].id
   },
   { 
   	body: "'hello'[2] => 'e'", 
   	correct: true, 
-  	question_id: Question.last.id
+  	question_id: @questions[2].id
   },
   { 
   	body: "'e'*3 => 'eee'", 
   	correct: true, 
-  	question_id: Question.last.id 
+  	question_id: @questions[2].id
   }
 ])
