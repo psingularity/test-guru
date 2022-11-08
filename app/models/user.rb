ebaf4a093ada80a9659cd96df124591e933058de
psingularity <lydia.alexandrowna@gmail.com>
2022-11-08 04:57:49 +0300

    style: used rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def user_tests(level)
     Test.joins('JOIN users_tests ON tests.id = users_tests.test_id')
-         .where('tests.level = :level AND users_tests.user_id = :user_id',
-                level: level, user_id: id)
+        .where('tests.level = :level AND users_tests.user_id = :user_id',
+               level: level, user_id: id)
   end
 end
