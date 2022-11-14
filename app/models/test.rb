444fd3bb804d307ec04f9f721c62ddef66a01523
psingularity <lydia.alexandrowna@gmail.com>
2022-11-15 02:46:46 +0300

    fix: added associations in models category and test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,14 @@
 class Test < ApplicationRecord
 
   has_many :questions
-
+  belongs_to :category
+  
+=begin
   def self.sort_test_titles_by_category(category)
     Test.joins('JOIN categories ON categories.id = tests.category_id')
         .where('categories.title = :category', category: category)
         .order('tests.title DESC')
         .pluck('tests.title')
   end
+=end
 end
