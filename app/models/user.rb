2c1faa664bbe7c18636a4addb73ab2d3a4d91586
psingularity <lydia.alexandrowna@gmail.com>
2022-11-15 06:00:07 +0300

    fix: removing unused code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,4 @@ class User < ApplicationRecord
 
   has_many :tests, inverse_of: 'author'
 
-=begin 
-
-  has_and_belongs_to_many :tests
-
-  def user_tests(level)
-    Test.joins('JOIN users_tests ON tests.id = users_tests.test_id')
-        .where('tests.level = :level AND users_tests.user_id = :user_id',
-               level: level, user_id: id)
-               
-  end  
-=end
 end
