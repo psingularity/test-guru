2d5875543cfd8ab665903ba8c4e04ebd7b1bf6ca
psingularity <lydia.alexandrowna@gmail.com>
2022-11-16 18:11:14 +0300

    fix: chanched model tests_users

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,4 +8,14 @@ class Test < ApplicationRecord
 
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+=begin
+  scope :by_category, -> (category) {joins(:category).where('categories.title = :category', category: category)} 
+
+  def self.sort_test_titles_by_category(category)
+    Test.joins('JOIN categories ON categories.id = tests.category_id')
+        .where('categories.title = :category', category: category)
+        .order('tests.title DESC')
+        .pluck('tests.title')
+  end
+=end
 end
---
+++ db/migrate/20221116000135_create_tests_users.rb
@@ -0,0 +1,10 @@
+class CreateTestsUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tests_users do |t|
+      t.references :user, foreign_key: { to_table: :users }
+      t.references :test, foreign_key: { to_table: :tests }
+
+      t.timestamps
+    end
+  end
+end
