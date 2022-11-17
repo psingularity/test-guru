1cf94280b7a2148f8ad00f9fe59a1d42a96a2686
psingularity <lydia.alexandrowna@gmail.com>
2022-11-17 04:02:32 +0300

    fix: editing the association between the test and the author who created it

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,13 +4,18 @@ class Test < ApplicationRecord
   belongs_to :category
 
   has_many :tests_users
-  has_many :users, through: :tests_users  
+  has_many :users, through: :tests_users 
 
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  scope :by_category, -> (category) {joins(:category).where(categories: { title: category }) } 
+
+  def self.sort_test_titles_by_category(category)
+    by_category(category).order(title: :DESC).pluck(:title)
+  end
+ 
 =begin
-  scope :by_category, -> (category) {joins(:category).where('categories.title = :category', category: category)} 
-
+  
   def self.sort_test_titles_by_category(category)
     Test.joins('JOIN categories ON categories.id = tests.category_id')
         .where('categories.title = :category', category: category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
-  has_many :tests, inverse_of: 'author'
+  has_many :authored_tests, class_name: 'Test', inverse_of: 'author'
+
+  def tests_by_level(level)
+    tests.where(level: level)
+  end
 
 end
