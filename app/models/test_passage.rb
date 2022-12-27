7e52d51765aa5b9ca1ad7d50901c1a9db545b5f8
psingularity <lydia.alexandrowna@gmail.com>
2022-12-28 00:33:41 +0300

    fix:assigning next question in TestPassage model using callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   SUCCESS_RATE = 85
 
@@ -15,10 +16,8 @@ class TestPassage < ApplicationRecord
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
-
-    self.current_question = next_question
     save!
-  end 
+  end
 
   def number_of_question
     test.questions.index(current_question) + 1
@@ -37,18 +36,24 @@ class TestPassage < ApplicationRecord
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
   end
-
+  
   def correct_answer?(answer_ids)
     correct_answers_count = correct_answers.count
-    (correct_answers.count == correct_answers.where(id: answer_ids).count) && correct_answers_count == answer_ids.count
+
+    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+      correct_answers_count == answer_ids.count
   end
 
   def correct_answers
     current_question.answers.correct
   end
 
- def next_question
+  def next_question
     test.questions.order(:id).where('id > ?', current_question.id).first 
+  end
+
+  def before_validation_set_next_question
+    self.current_question = next_question
   end
 
 end
